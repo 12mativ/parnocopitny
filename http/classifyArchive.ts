18d11fe57b4e774154a378d903ef27f15ab572c2
12mativ <ivan_m146@mail.ru>
2024-05-18 23:35:36 +0400

    fix3

--- http/classifyArchive.ts
+++ http/classifyArchive.ts
@@ -2,6 +2,6 @@ import axios from "axios"
 
 
 export const classifyArchive = async (formData: FormData) => {
-  const response = axios.post("http://localhost:8000/classify", formData);
+  const response = axios.post("http://localhost:8000/classify", formData, {responseType: "arraybuffer"});
   return response;
 }
